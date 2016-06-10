e0e408267ccf9da70ef4a1251b1e0be6da6aa40a
Rodrigo Nana F. Barbosa <rodrigonanafb@gmail.com>
2016-06-10 11:37:55 -0300

    Logs

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ var app = require('./server/config/express')();
 
 app.listen(3001,function(){
 	console.log("ok");
+	console.log("ok");
+	console.log("ok");
+	console.log("ok");
+	console.log("ok");
 });
